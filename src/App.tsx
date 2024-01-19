e6860c46867e7c4871fbf9dc822dc698cca288f5
Michael <l1ghtdev.88@gmail.com>
2024-01-18 23:57:37 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { Button, buttonVariants } from './components/ui/button'
-
-import {
-  type CarouselApi,
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselPrevious,
-  CarouselNext,
-} from './components/ui/carousel'
-
 function App() {
   return (
     <>
-      <Button>Click me</Button>
-
-      <Carousel>
-        <CarouselContent>
-          <CarouselItem className='basis-1/3'>item 1</CarouselItem>
-          <CarouselItem className='basis-1/3'>item 2</CarouselItem>
-          <CarouselItem className='basis-1/3'>item 3</CarouselItem>
-        </CarouselContent>
-      </Carousel>
+      <h1>Hello world</h1>
     </>
   )
 }
