ca3abb90193a61ca88a1f644b1a883ee2e0b440a
Michael <l1ghtdev.88@gmail.com>
2024-01-19 00:09:48 -0800

    radix test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { Theme } from '@radix-ui/themes'
 function App() {
   return (
     <>
       <h1>Hello world</h1>
+      <Theme>
+        <Dialog.Root>
+          <Dialog.Trigger>open</Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Title</Dialog.Title>
+              <Dialog.Description>Description</Dialog.Description>
+              <Dialog.Close>close</Dialog.Close>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </Theme>
     </>
   )
 }
